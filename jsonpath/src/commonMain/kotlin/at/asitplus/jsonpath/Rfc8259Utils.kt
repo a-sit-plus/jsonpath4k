d5b60f89b80ade8e1875d0b561fe336e9479ac24
acrusage <139230478+acrusage@users.noreply.github.com>
2024-04-26 12:06:24 +0200

    Change: Make companion object to object

--- jsonpath/src/commonMain/kotlin/at/asitplus/jsonpath/Rfc8259Utils.kt
+++ jsonpath/src/commonMain/kotlin/at/asitplus/jsonpath/Rfc8259Utils.kt
@@ -4,14 +4,12 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonPrimitive
 
-interface Rfc8259Utils {
-    companion object {
-        fun unpackStringLiteral(string: String): String {
-            return Json.decodeFromString<JsonPrimitive>(string).content
-        }
+object Rfc8259Utils {
+    fun unpackStringLiteral(string: String): String {
+        return Json.decodeFromString<JsonPrimitive>(string).content
+    }
 
-        fun escapeToDoubleQuotedString(string: String): String {
-            return Json.encodeToString(JsonPrimitive(string))
-        }
+    fun escapeToDoubleQuotedString(string: String): String {
+        return Json.encodeToString(JsonPrimitive(string))
     }
 }
