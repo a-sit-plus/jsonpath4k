c7dc75a4c949c93fb7de814f40aa1dc081313570
acrusage <139230478+acrusage@users.noreply.github.com>
2024-04-30 13:12:25 +0200

    Add: Sonatype publishing task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
+    alias(libs.plugins.gradle.nexus.publish)
 }
 
 repositories {
     maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
     mavenCentral()
     gradlePluginPortal()
+}
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
 }
 
 val artifactVersion: String by extra
